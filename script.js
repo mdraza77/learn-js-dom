918ea1a056981462a22d61a904ef7383c8941690
Md Raza <mdraza8397@gmail.com>
2026-01-13 21:55:14 +0530

    functional programming

--- script.js
+++ script.js
@@ -17,15 +17,21 @@
 //     }
 // });
 
-let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+// let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 // let b = a.toString();
 // let c = a.join(' and ');
 // console.log(c);
 // console.log(typeof b);
-a.pop();
-a.push(5);
-a.unshift(0);
-a.shift();
-a.splice(1, 5);
+// a.pop();
+// a.push(5);
+// a.unshift(0);
+// a.shift();
+// a.splice(1, 5);
 // a[-1] = 100;
-console.log(a);
+// console.log(a);
+
+function updateScore(score = 0) {
+    return score += 10;
+}
+
+console.log(updateScore(60));
