e3fb9175ce89510ff858924ed502af8ac154775a
Md Raza <mdraza8397@gmail.com>
2026-01-13 21:08:29 +0530

    DOM completed

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let bulb = document.querySelector('#bulb');
 let button = document.querySelector('#button');
+let h = document.querySelectorAll('h1');
 let bulbStatus = false;
+
+console.log(h);
 
 button.addEventListener('click', function () {
     if (bulbStatus === false) {
