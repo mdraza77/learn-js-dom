ed49c7b589dfb2d80b4df30d4bd39789f4924c94
Md Raza <mdraza8397@gmail.com>
2026-01-13 21:41:54 +0530

    array methods

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
-let bulb = document.querySelector('#bulb');
-let button = document.querySelector('#button');
-let h = document.querySelectorAll('h1');
-let bulbStatus = false;
+// let bulb = document.querySelector('#bulb');
+// let button = document.querySelector('#button');
+// let h = document.querySelectorAll('h1');
+// let bulbStatus = false;
 
-console.log(h);
+// console.log(h);
 
-button.addEventListener('click', function () {
-    if (bulbStatus === false) {
-        bulb.style.backgroundColor = 'yellow';
-        button.innerText = 'Off';
-        bulbStatus = true;
-    } else {
-        bulb.style.backgroundColor = 'gray';
-        button.innerText = 'On';
-        bulbStatus = false;
-    }
-});
+// button.addEventListener('click', function () {
+//     if (bulbStatus === false) {
+//         bulb.style.backgroundColor = 'yellow';
+//         button.innerText = 'Off';
+//         bulbStatus = true;
+//     } else {
+//         bulb.style.backgroundColor = 'gray';
+//         button.innerText = 'On';
+//         bulbStatus = false;
+//     }
+// });
+
+let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+// let b = a.toString();
+// let c = a.join(' and ');
+// console.log(c);
+// console.log(typeof b);
+a.pop();
+a.push(5);
+a.unshift(0);
+a.shift();
+a.splice(1, 5);
+// a[-1] = 100;
+console.log(a);
